38498b5fe6e2b5e2fdc7ad8116e9b3575e2deb08
Benjamin Cheng <lolzballs@teamfield3.tk>
2014-04-22 18:52:37 -0400

    Added .gitignore; Hid the command prompt window

--- Screenshot.cpp
+++ Screenshot.cpp
@@ -6,6 +6,9 @@
 	lpCmdLine = lcl;*/
 
 int main(){
+	HWND hWnd = GetConsoleWindow();
+	ShowWindow(hWnd, SW_HIDE);
+
 	hInstance = 0;
 	hPrevInstance = 0;
 	lpCmdLine = 0;
@@ -16,7 +19,7 @@ int main(){
 	ImageCodecInfo *pImageCodecInfo = (ImageCodecInfo *)(malloc(size));
 	GetImageEncoders(num, size, pImageCodecInfo);
 	for (unsigned int j = 0; j < num; ++j){
-		wcout << pImageCodecInfo[j].MimeType << endl;
+	wcout << pImageCodecInfo[j].MimeType << endl;
 	}*/
 
 	if (!registerWindowClass()){
